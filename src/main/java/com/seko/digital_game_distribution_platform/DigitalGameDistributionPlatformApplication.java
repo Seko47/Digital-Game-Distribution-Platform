f313ef1d9c8b74b476ce576e8cd9d644efdd1f0d
Seko47 <rafalsekular96@gmail.com>
2019-05-02 10:54:22 +0200

    Initial commit

---
+++ src/main/java/com/seko/digital_game_distribution_platform/DigitalGameDistributionPlatformApplication.java
@@ -0,0 +1,15 @@
+package com.seko.digital_game_distribution_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalGameDistributionPlatformApplication
+{
+
+	public static void main ( String[] args )
+	{
+		SpringApplication.run ( DigitalGameDistributionPlatformApplication.class, args );
+	}
+
+}
