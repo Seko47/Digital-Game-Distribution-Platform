6e7f968319b495975fe724d8f2e781114df6865f
Seko47 <rafalsekular96@gmail.com>
2019-05-03 13:15:47 +0200

    Add order controller

---
+++ src/main/java/com/seko/digital_game_distribution_platform/controllers/OrderController.java
@@ -0,0 +1,43 @@
+package com.seko.digital_game_distribution_platform.controllers;
+
+import com.seko.digital_game_distribution_platform.models.Order;
+import com.seko.digital_game_distribution_platform.services.OrderService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping ( "/orders" )
+public class OrderController
+{
+	private final OrderService orderService;
+
+	public OrderController ( OrderService orderService ) {this.orderService = orderService;}
+
+	@GetMapping
+	public List<Order> findAll ()
+	{
+		return this.orderService.findAll ();
+	}
+
+	@GetMapping ( "/{id}" )
+	public Order findById ( @PathVariable Long id )
+	{
+		return this.orderService.findById ( id );
+	}
+
+	@GetMapping ( "/buy/{id}" )
+	@ResponseStatus ( HttpStatus.CREATED )
+	public Order buy ( @PathVariable Long id )
+	{
+		return this.orderService.buy ( id );
+	}
+
+	@GetMapping ( "/has/{id}" )
+	@ResponseStatus ( HttpStatus.OK )
+	public Boolean hasUserGame ( @PathVariable Long id )
+	{
+		return this.orderService.hasGame ( id );
+	}
+}
